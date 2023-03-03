f2259653fd81903e4d81c5d8a0729c3432e9cbcb
FaizKuntoro <114470525+FaizKuntoro@users.noreply.github.com>
2023-03-03 15:40:47 +0700

    Add files via upload

---
+++ praktikum1/prak.php
@@ -0,0 +1,31 @@
+<?php
+// komentar
+/* komentar */
+# komentar
+
+echo "hello world <br> "; 
+echo 'hello world <br> '; 
+print_r("faiz kuntoro <br>");
+var_dump("STT NURUL FIKRI");
+echo "<hr>";
+
+$nama = "Faiz Kuntoro";
+$umur = 19;
+$berat = 65.2;
+$mahasiswa = true;
+
+echo "Nama saya $nama";    
+echo "<br> Umur saya adalah $umur tahun";
+echo "<br> Berat saya adalah $berat kg";
+echo "<hr>";
+
+
+//array
+
+$programs = ["java <br>" , "php <br>"];
+echo $programs[0];
+foreach ($programs as $program){
+    echo $program;
+}
+
+?>
